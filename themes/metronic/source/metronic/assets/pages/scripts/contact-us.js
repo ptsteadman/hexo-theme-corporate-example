362b3f046f1c525642060a92518bbe7eeda875a6
Patrick Steadman <ptsteadman@gmail.com>
2015-11-29 22:35:00 -0500

    Add contact form

--- themes/metronic/source/metronic/assets/pages/scripts/contact-us.js
+++ themes/metronic/source/metronic/assets/pages/scripts/contact-us.js
@@ -7,7 +7,7 @@ var ContactUs = function () {
 			$(document).ready(function(){
 			  map = new GMaps({
 				div: '#map',
-	            lat: -13.004333,
+	                        lat: -13.004333,
 				lng: -38.494333,
 			  });
 			   var marker = map.addMarker({
